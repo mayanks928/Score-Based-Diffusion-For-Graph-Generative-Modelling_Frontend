0932f23c1f774ed1ea3931e6e19b2df84f663b2d
Mayank Shetty <mayanks928@gmail.com>
2023-04-28 18:38:06 +0530

    Updated samples page

--- src/components/Samples.jsx
+++ src/components/Samples.jsx
@@ -1,19 +1,75 @@
-import React, { useState } from "react";
+import React from "react";
 import "../styles/samples.css";
-import { TextField, Button } from "@mui/material";
 import { motion } from "framer-motion";
 
 export default function Samples() {
+  const smilesData = [
+    {
+      name: "CC1CC(CCN)O1",
+      path: "1",
+    },
+    {
+      name: "NC=NC1=C(O)N1",
+      path: "2",
+    },
+    {
+      name: "COOC1(O)CC1(C)O",
+      path: "3",
+    },
+    {
+      name: "C=C1CN=C2CC1(C)N2",
+      path: "4",
+    },
+    {
+      name: "OCCC=C1C=CN=C1",
+      path: "5",
+    },
+    {
+      name: "C#CCOC(=N)CCC",
+      path: "6",
+    },
+    {
+      name: "CCN(O)C=NC(C)=O",
+      path: "7",
+    },
+    {
+      name: "CCC1(N)CC=CN1",
+      path: "8",
+    },
+    {
+      name: "CC1CC1OOC(N)=O",
+      path: "9",
+    },
+    {
+      name: "CC12CC34C=NC31C2C4",
+      path: "10",
+    },
+  ];
 
   return (
-    <motion.div className="content"
-    initial={{x:"-100%"}}
-    animate={{x:0,transition:{duration:0.2,delay:0.2}}}
-    exit={{y:window.innerHeight,transition:{duration:0.2}}}>
+    <motion.div
+      className="content"
+      initial={{ x: "-100%" }}
+      animate={{ x: 0, transition: { duration: 0.2, delay: 0.2 } }}
+      exit={{ y: window.innerHeight, transition: { duration: 0.2 } }}
+    >
       <div className="samplescard">
         <h1>Generated Samples:</h1>
-        <p>Below are the generated SMILES strings and their corresponding graphs:</p>
-        <h2>Output:</h2>
+
+        <p>
+          Below are the generated SMILES strings and their corresponding graphs:
+        </p>
+        <div className="smilesImages">
+          {smilesData.map((individualSmile) => {
+            return (
+              <div className="individualSmile" key={individualSmile.path}>
+                <p>{individualSmile.name}</p>
+                <img src={`/samples/${individualSmile.path}.png`} />
+              </div>
+            );
+          })}
+        </div>
+        <p>Click <a>here</a> to download the model weights </p>
       </div>
     </motion.div>
   );
